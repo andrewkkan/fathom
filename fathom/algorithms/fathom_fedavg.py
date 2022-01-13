1. Factorized the code a bit;  2. Corrected mistake in eta_c hyper step calculation.

fathom/algorithms/fathom_fedavg.py
@@ -52,7 +52,7 @@ def create_train_for_each_client(grad_fn, client_optimizer):
 
     def client_init(shared_input, client_rng):
         opt_state = client_optimizer.init(shared_input['params'])
-        opt_state.hyperparams['learning_rate'] = shared_input['eta_c']
+        opt_state.hyperparams['learning_rate'] = shared_input['eta_c'] # Already sigmoided
         client_step_state = {
             'params': shared_input['params'],
             'opt_state': opt_state,
@@ -85,11 +85,17 @@ def create_train_for_each_client(grad_fn, client_optimizer):
 
 
 @dataclasses.dataclass
+class Hyperparams:
+    eta_c: float    # Local learning rate or step size
+    tau: float      # Number of local steps
+    alpha: float    # Momentum for global gradient estimate
+    bs: float       # Local batch size
+
+
+@dataclasses.dataclass
 class MetaState:
     grad_glob: Params 
-    eta_c: float # eta_c from t
-    _eta_c: float # eta_c from t-1
-    gradsum_inst: Params
+    hyperparams: Hyperparams 
     opt_state: optimizers.OptState
 
 
@@ -113,6 +119,8 @@ def federated_averaging(
         server_optimizer: optimizers.Optimizer,
         hyper_optimizer: optimizers.Optimizer,
         client_batch_hparams: client_datasets.ShuffleRepeatBatchHParams,
+        # eta_hyper: Hyperparams,  # Learning rates.  For statically set hyperparams, set learning rates to 0.
+        server_init_hparams: Hyperparams,
 ) -> federated_algorithm.FederatedAlgorithm:
     """Builds federated averaging.
 
@@ -131,13 +139,19 @@ def federated_averaging(
     def init(params: Params) -> ServerState:
         opt_state_server = server_optimizer.init(params)
         opt_state_client = client_optimizer.init(params) # Just to access hyperparams for eta_c
-        eta_c = opt_state_client.hyperparams['learning_rate']
-        opt_state_hyper = hyper_optimizer.init(-jnp.log(1. / eta_c - 1.))
+        assert(server_init_hparams.eta_c == opt_state_client.hyperparams['learning_rate'])
+        assert(server_init_hparams.tau == float(client_batch_hparams.num_steps))
+        assert(server_init_hparams.bs == float(client_batch_hparams.batch_size))
+        hyperparams = Hyperparams(
+            eta_c = -jnp.log(1. / server_init_hparams.eta_c - 1.),
+            tau = float(server_init_hparams.tau) - 1.,
+            alpha = server_init_hparams.alpha,
+            bs = float(server_init_hparams.bs) - 1.,
+        )
+        opt_state_hyper = hyper_optimizer.init(hyperparams)
         meta_state = MetaState(
             grad_glob = tree_util.tree_zeros_like(params), # grad_glob 
-            eta_c = eta_c,
-            _eta_c = eta_c,
-            gradsum_inst = tree_util.tree_zeros_like(params),
+            hyperparams = hyperparams,
             opt_state = opt_state_hyper,
         )
         # Need to initialize round_index to 1 for bias comp
@@ -157,9 +171,17 @@ def federated_averaging(
         ]],
     ) -> Tuple[ServerState, Mapping[federated_data.ClientId, Any]]:
         client_num_examples = {cid: len(cds) for cid, cds, _ in clients}
-        batch_clients = [(cid, cds.shuffle_repeat_batch(client_batch_hparams), crng)
+        current_client_batch_hparams = client_datasets.ShuffleRepeatBatchHParams(
+            batch_size = int(jax.nn.relu(server_state.meta_state.hyperparams.bs)+1.5), # In practice this is pushed to clients
+            num_steps = int(jax.nn.relu(server_state.meta_state.hyperparams.tau)+1.5), # In practice this is pushed to clients 
+            num_epochs = client_batch_hparams.num_epochs,
+            drop_remainder = client_batch_hparams.drop_remainder,
+            seed = client_batch_hparams.seed,
+            skip_shuffle = client_batch_hparams.skip_shuffle,
+        )
+        batch_clients = [(cid, cds.shuffle_repeat_batch(current_client_batch_hparams), crng)
                                          for cid, cds, crng in clients]
-        shared_input = {'params': server_state.params, 'eta_c': server_state.meta_state.eta_c}
+        shared_input = {'params': server_state.params, 'eta_c': jax.nn.sigmoid(server_state.meta_state.hyperparams.eta_c)}
         client_diagnostics = {}
         # Running weighted mean of client updates. We do this iteratively to avoid
         # loading all the client outputs into memory since they can be prohibitively
@@ -186,8 +208,8 @@ def federated_averaging(
             server_state.opt_state, 
             server_state.params,
         )
-        gradsum_inst = tree_util.tree_inverse_weight(mean_delta_params, server_state.meta_state.eta_c)
-        alpha = jnp.array(0.9)
+        gradsum_inst = tree_util.tree_inverse_weight(mean_delta_params, jax.nn.sigmoid(server_state.meta_state.hyperparams.eta_c))
+        alpha = jax.nn.relu(server_state.meta_state.hyperparams.alpha)
         grad_glob = jax.tree_util.tree_multimap(lambda a, b: (
                 (a * alpha + b * (1. - alpha)) #/ # grad_glob with momentum noise filtering
                 #(1. - alpha ** server_state.round_index) # bias comp
@@ -195,30 +217,34 @@ def federated_averaging(
             server_state.meta_state.grad_glob,
             gradsum_inst,
         )
-        hyper_optate, eta_c = hyper_optimizer.apply(
-            hyper_step(server_state, grad_glob), 
+        hyper_optate, hyperparams = hyper_optimizer.apply(
+            hyper_step(server_state, gradsum_inst, grad_glob), 
             server_state.meta_state.opt_state, 
-            -jnp.log(1. / server_state.meta_state.eta_c - 1.)
+            server_state.meta_state.hyperparams,
         )
         meta_state = MetaState(
             grad_glob = grad_glob, 
-            eta_c = jax.nn.sigmoid(eta_c),
-            _eta_c = server_state.meta_state.eta_c,
-            gradsum_inst = gradsum_inst,
+            hyperparams = hyperparams,
             opt_state = hyper_optate,
         )
         return ServerState(params, opt_state, server_state.round_index + 1, meta_state)
 
     def hyper_step(
         server_state: ServerState,
+        gradsum_inst: Params,
         grad_glob: Params,
-    ) -> float:
-        sigmoid_prime = jax.grad(jax.nn.sigmoid)(server_state.meta_state._eta_c)
-        sigmoid_primeprime = jax.grad(jax.grad(jax.nn.sigmoid))(server_state.meta_state._eta_c)
-        cossim = jax.tree_util.tree_multimap(lambda a, b: a * -b, grad_glob, server_state.meta_state.gradsum_inst)
+    ) -> Hyperparams:
+
+        sigmoid_prime = jax.grad(jax.nn.sigmoid)(server_state.meta_state.hyperparams.eta_c)
+        cossim = jax.tree_util.tree_multimap(lambda a, b: a * -b, grad_glob, gradsum_inst)
         cossim_flatten = jax.flatten_util.ravel_pytree(cossim)
-        cossim_mean = jnp.sum(cossim_flatten[0])
-        hyper_step = (sigmoid_prime + sigmoid_primeprime * (server_state.meta_state.eta_c - server_state.meta_state._eta_c)) * cossim_mean
+        cossim_agg = jnp.sum(cossim_flatten[0])
+        eta_c_step = sigmoid_prime * cossim_agg 
+
+        relu_prime = jax.grad(jax.nn.relu)(server_state.meta_state.hyperparams.tau)
+        tau_step = relu_prime * 0.0 
+
+        hyper_step = Hyperparams(eta_c = eta_c_step, tau = tau_step, alpha = 0.0, bs = 0.0)
         return hyper_step
 
     return federated_algorithm.FederatedAlgorithm(init, apply)